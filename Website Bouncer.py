1da8c04c33180f50bdde5433ebd32beaab675ea1
ChikenNuget1 <160695520+ChikenNuget1@users.noreply.github.com>
2025-01-24 14:33:50 +1300

    Update Website Bouncer.py
    Added way to obtain every link out of a wikipedia article except for
     - /wiki/Category/ links
    - /wiki/Help/ links
    - /wiki/Special/ links
    - /wiki/Template/ links
    - /wiki/File/ links
    I have decided to omit these links as I don't think they are the main links that people want when they visit a Wikipedia article. They just needlessly bloat  up the final list at the end so I have decided to just add the pure /wiki/ links which go to other Wikipedia articles.

--- Website Bouncer.py
+++ Website Bouncer.py
@@ -4,11 +4,17 @@ Delete any duplicates which show up
 Try to see if you can make a general web scraper that works with most websites.
 """
 
+"""
+Goals for tomorrow:
+Add a helper function to get rid of any duplicate codes
+    - I see one or two duplicate pieces of code in this file. See if you can create helper functions to get rid of these.
+"""
+
 from bs4 import BeautifulSoup
 import requests
 import random
 
-def websiteScraper(url, max=10, current=0):
+def wikipedia_scraper(url, max=10, current=0):
     """
     Scrape wikipedia starting from given URL. Keep bouncing between URLs.
 
@@ -26,7 +32,7 @@ def websiteScraper(url, max=10, current=0):
 
     # Find the title
     title = soup.find(id="firstHeading")
-    print(title.text)
+    print(f"{title.text}")
 
     # Find all <a> within the bodyContent of the artical
     all_links = soup.find(id="bodyContent").find_all('a')
@@ -40,7 +46,7 @@ def websiteScraper(url, max=10, current=0):
         break
     try:
         current += 1
-        websiteScraper("https://en.wikipedia.org" + linktoscrape['href'], max, current)
+        wikipedia_scraper("https://en.wikipedia.org" + linktoscrape['href'], max, current)
     except KeyError:
         raise KeyError("eiuewhiawrh")
 
@@ -81,8 +87,39 @@ def lol_scraper(url, max=10, current=0):
         raise KeyError("wiaohioh")
 
 
+def all_link_scraper(url, max=10, current=0):
+    """
+    Scrape wikipedia starting from given URL. Keep bouncing between URLs.
+
+    Keyword arguments:
+    url -- Given URL that is the start of the bouncing.
+    """
+
+    # Get the HTML code for the URL
+    response = requests.get(url=url, timeout=10)
+    soup = BeautifulSoup(response.content, "html.parser")
+    link_list = []
+    # Find all <a> within the bodyContent of the artical
+    all_links = soup.find(id="bodyContent").find_all('a')
+    for link in all_links:
+        # Find <a> with <href> that contains 'wiki'
+        try:
+            # Such long IF statements man. They're so long I had to split them in 2.
+            # This shit is not efficient at all >_<
+            # Oh well, This is only a for-fun project anyways.
+            if link['href'] not in link_list and link['href'].find('/wiki/') != -1 and link['href'].find('/wiki/Category') == -1 and link['href'].find('/wiki/Help') == -1:
+                if link['href'].find('/wiki/Special') == -1 and link['href'].find('/wiki/Template') == -1 and link['href'].find('/wiki/File') == -1:
+                    link_list.append(link['href'])
+        except KeyError:
+            pass
+    
+    for i in link_list:
+        print(i[6:])
+
+
+
 def website_print():
-    website_list = ["lol.fandom.com", "wikipedia.org"]
+    website_list = ["lol.fandom.com", "wikipedia.org", "wikipedia.org but obtain list of every link"]
     for i in range(len(website_list)):
         print(f"{i + 1}, {website_list[i]}")
 
@@ -103,7 +140,10 @@ def user_input():
             lol_scraper(user_url)
         case 2:
             user_url = input("Please enter a wikipedia url: ")
-            websiteScraper(user_url)
+            wikipedia_scraper(user_url)
+        case 3:
+            user_url = input("Please enter a wikipedia url: ")
+            all_link_scraper(user_url)
 
 
 if __name__ == "__main__":
